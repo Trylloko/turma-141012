82bef55e918e0dc2b96690a8baccf6eb863be69e
Dyego Silva <dyegosilva90@gmail.com>
2022-09-06 21:51:32 -0300

    Montando o carrinho com os botoes add

--- aulas/projeto-0824/js/script.js
+++ aulas/projeto-0824/js/script.js
@@ -1,17 +1,19 @@
 let xhttp = new XMLHttpRequest();
 let lsProduto = [];
 
-function buscarProduto(){
-    xhttp.open("GET","https://pascoa-chiquinha.herokuapp.com/produto/");
+function buscarProduto() {
+    xhttp.open("GET", "https://pascoa-chiquinha.herokuapp.com/produto/");
     xhttp.send();
-    xhttp.onload= function (){
+    xhttp.onload = function () {
         lsProduto = this.response;
         lsProduto = JSON.parse(lsProduto);
         montarListaProdutosHtml(lsProduto);
+        marcarProdutosSelecionadosLocalStorage();
     }
 }
 
-function montarListaProdutosHtml(lsProduto){
+
+function montarListaProdutosHtml(lsProduto) {
     let listaProduto = "";
     let i = 0;
     for (produto of lsProduto) {
@@ -31,25 +33,65 @@ function montarListaProdutosHtml(lsProduto){
     document.getElementById("listaProduto").innerHTML = listaProduto;
 }
 
-function addProdutoCarrinho(i){
+function marcarProdutosSelecionadosLocalStorage() {
+    let lista = localStorage.getItem("listaProdutoLocalStorage");
+    lista = JSON.parse(lista);
+    for (i in lista) {
+        if (lista[i].carrinho) {
+            addProdutoCarrinho(i);
+        }
+    }
+}
+function addProdutoCarrinho(i) {
     let produto = lsProduto[i];
-    if(produto.carrinho == false){
+    if (produto.carrinho == false) {
         produto.carrinho = true;
         //console.log(produto);
-        document.getElementsByClassName("carrinho")[i].style.color = "#e66b6b";    
-    }else{
+        document.getElementsByClassName("carrinho")[i].style.color = "#e66b6b";
+    } else {
         produto.carrinho = false;
         document.getElementsByClassName("carrinho")[i].style.color = "#0000007d";
     }
-   
-    function verListaProdutoSelecionado(){
-        for (produto of lsProduto) {
-            if(produto.carrinho){
-                console.log(produto)
-            }
+    localStorage.setItem("listaProdutoLocalStorage", JSON.stringify(lsProduto));
+}
+
+function verListaProdutosSelecionado() {
+    let listaProduto = "";
+    document.getElementById("listaProduto").innerHTML = "";
+    let i = 0;
+    let j = 0;
+    for (produto of lsProduto) {
+        if (produto.carrinho) {
+            produto.quantidade = 1;
+            listaProduto += `
+        <div class="embrulho">
+            <div class="produto">
+                <img src="${produto.imagem}" alt="">
+                <p> ${produto.nome};
+                    <span class="valor">${produto.valor.toFixed(2)}</span>
+                </p>
+                <span class="btMaisMenos">
+                <span class="btMais" onclick="add(1,${i},${j})">+ </span>
+                <span class="btMenos" onclick="add(-1,${i},${j})">- </span>
+                </span>
+                <span class="quantidade">${produto.quantidade}</span>
+            </div>
+        </div>`
+                ;
+            i++;
         }
+        j++;
     }
+    document.getElementById("listaProduto").innerHTML = listaProduto;
 
 }
+
+function add(qt, i, j) {
+    // console.log(qt+" "+i);
+    // console.log(lsProduto[j]);
+    lsProduto[j].quantidade +=qt;
+    document.getElementsByClassName("quantidade")[i].innerHTML = lsProduto[j].quantidade;
+}
+
 
 buscarProduto();
