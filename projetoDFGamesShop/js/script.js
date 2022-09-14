c237669fd5be676dfa1045be585ae315d35d5df1
Dyego Silva <dyegosilva90@gmail.com>
2022-09-14 20:21:46 -0300

    Carregar lista produtos API

--- projetoDFGamesShop/js/script.js
+++ projetoDFGamesShop/js/script.js
@@ -1,1 +1,30 @@
 let xhttp = new XMLHttpRequest();
+
+function acessarProduto() {
+    xhttp.open("GET", "https://dfgamesshop.herokuapp.com/produto/");
+    xhttp.send();
+    xhttp.onload = function () {
+        let lsProduto = this.response;
+        lsProduto = JSON.parse(lsProduto);
+        montarListaConsoles(lsProduto);
+    }
+}
+
+function montarListaConsoles(lsProduto) {
+    let listaProduto = "";
+    for (produto of lsProduto) {
+        listaProduto += `
+        <div class="embrulho">
+        <div class="produto">
+            <img src="${produto.imagem}" alt="">
+            <p>${produto.nome}
+               <span class="preco">${produto.valor.toFixed(2)}</span> 
+            </p>
+            <i class="material-icons">&#xe8cb;</i>
+        </div>
+        </div>
+        `;
+    }
+    document.getElementById("listaConsoles").innerHTML = listaProduto;
+}
+acessarProduto();
